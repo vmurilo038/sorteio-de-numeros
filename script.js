61515992521ab23a3500df56092575c35134f8fb
Murilo <vmurilo038@gmail.com>
2026-09-01 10:39:35 -0300

    Site para sorteio de números

---
+++ script.js
@@ -0,0 +1,30 @@
+function sortear() {
+
+    let minimo = Number(document.getElementById("minimo").value);
+    let maximo = Number(document.getElementById("maximo").value);
+
+    if (document.getElementById("minimo").value === "" ||
+        document.getElementById("maximo").value === "") {
+
+        alert("Digite o número mínimo e máximo!");
+        return;
+    }
+
+    if (minimo > maximo) {
+
+        alert("O número mínimo deve ser menor que o máximo!");
+        return;
+    }
+
+     if (minimo <= 0) {
+
+        alert("O número mínimo deve ser positivo!");
+        return;
+    }
+
+    let numeroSorteado = Math.floor(
+        Math.random() * (maximo - minimo + 1)
+    ) + minimo;
+
+    document.getElementById("numero").textContent = numeroSorteado;
+}
